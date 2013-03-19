b8e2963d14e5185bb51c0f6b6e0d6a8bdaf0569a
David Cuartielles <david@Talkoo.(none)>
2013-03-19 23:42:24 +0100

    modified KnockKnockBox to work with A0

--- Arduino/libraries/Castilla/examples/Magic/KnockKnockBox/KnockKnockBox.ino
+++ Arduino/libraries/Castilla/examples/Magic/KnockKnockBox/KnockKnockBox.ino
@@ -15,7 +15,7 @@
 //The number of knocks can be recorded
 #define MAX_KNOCKS 30
 
-PiezoKnockSensor sensor=PiezoKnockSensor(A5);
+PiezoKnockSensor sensor=PiezoKnockSensor(A0);
 Melody speaker=Melody(8);
 
 //An array for remembering the knock pattern
